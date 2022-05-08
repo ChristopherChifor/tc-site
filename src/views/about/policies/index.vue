9b0ba916256fb6bad08561a0bc63152b7b57bb1e
Payam Fakoorziba <payam.fakoorziba@mail.utoronto.ca>
2022-05-07 21:49:50 -0400

    Added page

--- src/views/about/policies/index.vue
+++ src/views/about/policies/index.vue
@@ -1,20 +1,49 @@
 <template>
   <Header title='about/policies' />
   
+  <!-- Container -->
+  <Container>
+    <div class="grid gap-7">
+      <div
+      v-for="value in values"
+      :key="value"
+      class="flex items-center"
+      >
+        <img src="@/assets/icons/document-icon.svg " alt="" />
+        <h3 class="text-xl text-center mb-0 ml-2">{{ value }}</h3>
+      </div>
+    </div>
+  </Container>
   
 </template>
 
+
+
 <script>
+import Container from '../../../components/layouts/Container.vue'
 import Header from "@/components/base/Header";
 
-
+const values = [
+  "Mission Statement",
+  "Vision Statement",
+  "Statement of Value",
+  "Intellectual Freedom",
+  "Respect and Acknowledgement Declaration",
+];
+    
     
 
 export default {
   components: {
-    Header,
+    Header, Container
     
   },
+
+  data() {
+    return {
+      values,
+    };
+  }
   
 };
 </script>
--- src/views/about/staff/index.vue
+++ src/views/about/staff/index.vue
@@ -1,20 +1,70 @@
 <template>
   <Header title='about/staff' />
   
+  <Container>
+    <h2 class="text-center font-bold">Staff Members</h2>
+    <div class="flex flex-wrap justify-evenly">
+      <div
+        v-for="name in staffNames"
+        :key="name"
+        class="flex flex-col items-center justify-center flex-shrink-0"
+      >
+        <img src="@/assets/images/staff-lady.png" alt="Staff member" class="w-16 h-16 mb-4 rounded-full object-cover" />
+        <h3 class="text-xl w- text-center text-accent">{{ name }}</h3>
+        <p class = "text-center -mt-6 ">Position title</p>
+      </div>
+    </div>
+  </Container>
+
+  <Container class="bg-accent-lightest">
+    <h2 class="text-center font-bold">Library Board Members</h2>
+    <div class="flex flex-wrap justify-evenly">
+      <div
+        v-for="name in boardNames"
+        :key="name"
+        class="flex flex-col items-center justify-center flex-shrink-0"
+      >
+        <img src="@/assets/images/staff-lady.png" alt="Staff member" class="w-16 h-16 mb-4 rounded-full object-cover" />
+        <h3 class="text-xl w- text-center text-accent">{{ name }}</h3>
+        <p class = "text-center -mt-6 t">Position title</p>
+      </div>
+    </div>
+  </Container>
   
 </template>
 
 <script>
+import Container from '../../../components/layouts/Container.vue'
 import Header from "@/components/base/Header";
 
+const staffNames = [
+  "Staff name",
+  "Staff name",
+  "Staff name",
+  "Staff name",
+  "Staff name",
+  "Staff name",
+];
 
+const boardNames = [
+  "Board Title",
+  "Board Title",
+  "Board Title",
+  "Board Title",
+  "Board Title",
+  "Board Title",
+]
     
 
 export default {
   components: {
-    Header,
-    
+    Header, Container
   },
-  
+  data() {
+    return {
+      staffNames,
+      boardNames
+    };
+  },
 };
 </script>
