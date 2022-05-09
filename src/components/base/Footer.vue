c18652f1fe07562a6b80cea47da07d496c912768
ChristopherChifor <christopher_chifor@yahoo.ca>
2022-05-09 16:55:09 -0400

    working on footer

--- src/components/base/Footer.vue
+++ src/components/base/Footer.vue
@@ -1,61 +1,103 @@
 <template>
-  <footer class="bg-accent" aria-labelledby="footer-heading">
-    <h2 id="footer-heading" class="sr-only">Footer</h2>
+  <footer class="bg-accent text-white" aria-labelledby="footer-heading">
+    <div class="todo">MAKE ALL TEXT WHITE and figure out the bottom color</div>
+    <h2 class="sr-only">Footer</h2>
     <div class="max-w-7xl mx-auto py-12 pb-8 px-4 sm:px-6 lg:px-8">
-      <div class="grid md:grid-cols-2 gap-8">
-        <router-link to="/">
-          <img class="h-9 w-auto" src="@/assets/logo.svg" alt="Company Name" />
-        </router-link>
-        <div class="grid sm:grid-cols-2 gap-8">
-          <div>
-            <h3 class="mb-3 footer-heading">Navigation</h3>
-            <div class="space-y-1">
-              <router-link
-                v-for="route in navigation"
-                :key="route.label"
-                class="
-                  block
-                  with-underline
-                  w-max
-                  transition
-                  hover:text-accent
-                  capitalize
-                "
-                :to="route.path"
-                >{{ route.label }}</router-link
-              >
+      <div class="grid sm:grid-cols-4 gap-24">
+        <div>
+          <h3 class="mb-3 footer-heading text-white">Hours Of Operation</h3>
+          <div class="space-y-1">
+            <div>
+              <p class="text-white"><b>Sunday:</b> Closed</p>
+              <p class="text-white"><b>Monday:</b> Closed</p>
+              <p class="text-white"><b>Tuesday:</b> 9:00am - 6:00pm</p>
+              <p class="text-white"><b>Wednesday:</b> 9:00am - 7:00pm</p>
+              <p class="text-white"><b>Thursday:</b> 9:00am - 6:00pm</p>
+              <p class="text-white"><b>Friday:</b> 9:00am - 6:00pm</p>
+              <p class="text-white"><b>Saturday:</b> 9:00am - 2:00pm</p>
             </div>
+            <!-- <router-link
+              v-for="route in navigation"
+              :key="route.label"
+              class="
+                block
+                with-underline
+                w-max
+                transition
+                hover:text-accent
+                capitalize
+              "
+              :to="route.path"
+              >{{ route.label }}</router-link
+            > -->
           </div>
-          <div>
-            <h3 class="mb-3 footer-heading">Contact Us</h3>
-            <div class="space-y-4">
-              <address class="not-italic">
-                Street <br />
-                City, Province POSTAL CODE
-              </address>
-              <a
-                href="tel:+19056379255"
-                class="block with-underline w-max hover:text-accent"
-                >Phone: xxx-xxx-xxxx
-              </a>
-              <a
-                href="mailto:info@teccweb.com"
-                class="block with-underline w-max hover:text-accent"
-                >Email: email@email.com</a
-              >
-            </div>
+        </div>
+        <div class="grid max-h-5">
+          <div class="todo">
+            figure out why grid doesnt work/ is this implementation okay?
+          </div>
+          <h3 class="footer-heading">Quick Links</h3>
+          <div class="space-y-1">
+            <a href="#">Announcements</a> <br />
+            <a href="#">Online Catalouge</a> <br />
+            <a href="#">eResources</a> <br />
+            <a href="#">Library Card</a> <br />
+            <a href="#">Donations</a> <br />
+          </div>
+        </div>
+        <div class="text-white">
+          <h3 class="mb-3 footer-heading text-white">Contact Us</h3>
+          <div class="space-y-1">
+            <p>Teck Centennial Public Library</p>
+            <p>10 Kirkland St E</p>
+            <p>Kirkland Lake, ON. P2N 1P1</p>
+            <a href="#">library@tkl.ca</a> <br />
+            <a href="#">705-567-7966</a>
+            <p>705-567-7966</p>
+            <!-- <router-link
+              v-for="route in navigation"
+              :key="route.label"
+              class="
+                block
+                with-underline
+                w-max
+                transition
+                hover:text-accent
+                capitalize
+              "
+              :to="route.path"
+              >{{ route.label }}</router-link
+            > -->
+          </div>
+        </div>
+        <div>
+          <h3 class="mb-3 footer-heading">Social Media</h3>
+          <div class="grid grid-row-1 grid-flow-col">
+            <a href="#">
+              <img src="/icons/facebook.svg" alt="" />
+            </a>
+            <a href="#"> <img src="/icons/instagram.svg" alt="" /> </a>
+            <a href="#">
+              <img src="/icons/twitter.svg" alt="" />
+            </a>
+            <a href="#">
+              <img src="/icons/youtube.svg" alt="" />
+            </a>
           </div>
         </div>
       </div>
+
       <div
         class="
           mt-8
           pt-8
           border-t border-gray-200
           md:flex md:items-center md:justify-between
+          bg-accent-dark
+          w-full
         "
       >
-        <p class="text-base text-gray-400">
+        <p class="text-base text-white">
           Teck Centennial Public Library &copy; 2022
         </p>
       </div>
